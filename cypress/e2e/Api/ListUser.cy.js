5a13f445fad955c64be4d19182346b9ec4bd5fc2
Shabnam Roohi <shabnam.roohy92@gmail.com>
2024-02-03 18:04:42 +0100

    make a mistake for test

--- cypress/e2e/Api/ListUser.cy.js
+++ cypress/e2e/Api/ListUser.cy.js
@@ -1,7 +1,7 @@
 describe('API Tests', () => {
     it('should make a GET request and assert the response', () => {
         // Make a GET request to the API endpoint
-        cy.request('https://reqres.in/api/users?page=2')
+        cy.request('https://reqrgffchfhfges.in/api/users?page=2')
             .should((response) => {
                 expect(response.status).to.eq(200)
             })
